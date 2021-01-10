dbb357c035d482439a10fb37e04534198e07c58f
impimsss <PimKyoya@gmail.com>
2021-01-10 23:31:59 +0700

    html

---
+++ flaskr/blog.py
@@ -0,0 +1,26 @@
+#Import necessary libraries
+from flask import Flask, render_template, redirect, url_for, request
+import cv2
+#Initialize the Flask app
+app = Flask(__name__, static_folder='static')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
+@app.route('/camera')
+def camera():
+    return render_template('camera.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
