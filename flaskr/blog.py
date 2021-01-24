c1271fc5047f19d27f7a68d306bad0e2bec7550f
impimsss <PimKyoya@gmail.com>
2021-01-25 05:00:20 +0700

    session

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,74 +1,69 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 import cv2
 import pyodbc
 
-def execute_sql(query):
-    connection = pyodbc.connect('Driver={SQL Server};'
-                      'Server=.;'
-                      'Database=IncidentRecord;'
-                      'Trusted_Connection=yes;')
-
-    cursor = connection.cursor()
-    cursor.execute(query)
-
-    return cursor.fetchall()
-
-def execute_sql_parameter(query, user, psw):
-    connection = pyodbc.connect('Driver={SQL Server};'
-                      'Server=.;'
-                      'Database=IncidentRecord;'
-                      'Trusted_Connection=yes;')
-
-    cursor = connection.cursor()
-    cursor.execute(query, user, psw)
-
-    return cursor.fetchall()
+connection = pyodbc.connect('Driver={SQL Server};'
+                    'Server=.;'
+                    'Database=IncidentRecord;'
+                    'Trusted_Connection=yes;')
+cursor = connection.cursor()
 
 
 app = Flask(__name__, static_folder='static')
+app.secret_key = '1a2b3c4d5e'
 
 
 @app.route('/login', methods=['POST'])
 def login():
     error = None
     if request.method == 'POST':
-        user = request.form['username']
-        psw = request.form['password']
+        username = request.form['username']
+        password = request.form['password']
 
-        userInfo = execute_sql_parameter('SELECT UserName, Password, UserType FROM [User] WHERE UserName = ? AND Password = ?' , user, psw)
-        # trysql = execute_sql('SELECT UserName, Password, UserType FROM [User] WHERE UserName = ?', user)
+        # userInfo = execute_sql_parameter('SELECT UserName, Password, UserType FROM [User] WHERE UserName = ? AND Password = ?' , user, psw)
+        account = cursor.execute('SELECT * FROM [User] WHERE userName = ? AND password = ?', (username, password))
+        account = cursor.fetchone()
 
-        for i in userInfo:
-            userName = i[0]
-            userPassword = i[1]
-            userType = i[2]
+        # for i in userInfo:
+        #     userName = i[0]
+        #     userPassword = i[1]
+        #     userType = i[2]
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['userName'] = account['userName']
+            session['password'] = account['password']
+            if(account[5] == 'Admin'):
+                return render_template('home.html')
+            else:
+                return render_template('userHome.html')
 
-        if user != userName or psw != userPassword:
-            error = 'Invalid Username or password. Please try again.'
-        elif userType == 'Admin':
-            return redirect(url_for('home'))
-        else :
-            return redirect(url_for('user'))
+        
 
-    return render_template('login.html', error=error)
+    #     elif userType == 'Admin':
+    #         return redirect(url_for('home'))
+    #     else :
+    #         return redirect(url_for('user'))
 
-        # user = cursor.execute('SELECT * FROM [User] WHERE UserName = %s AND Password = %s', username, password)
-        # print(user)
+    # return render_template('login.html', error=error)
+
+    #     # user = cursor.execute('SELECT * FROM [User] WHERE UserName = %s AND Password = %s', username, password)
+    #     # print(user)
 
 
 
-        # # If account exists in accounts table in out database
-        # if account:
-        #     # Create session data, we can access this data in other routes
-        #     session['loggedin'] = True
-        #     session['id'] = account['id']
-        #     session['username'] = account['username']
+    #     # If account exists in accounts table in out database
+    #     # if account:
+    #         # Create session data, we can access this data in other routes
+    #         session['loggedin'] = True
+    #         session['id'] = account['id']
+    #         session['username'] = account['username']
 
-        #     # Redirect to home page
-        #     return 'Logged in successfully!'
-        # else:
-        #     # Account doesnt exist or username/password incorrect
-        #     msg = 'Incorrect username/password!'
+    #     #     # Redirect to home page
+    #     #     return 'Logged in successfully!'
+    #     # else:
+    #     #     # Account doesnt exist or username/password incorrect
+    #     #     msg = 'Incorrect username/password!'
 
 @app.route('/blacklist')
 def blacklist():
@@ -81,17 +76,15 @@ def user():
 
 @app.route('/')
 def home():
-    historyData = execute_sql('Select H.HistoryID, C.LicensePlate, H.EnterTimestamp, H.ExitTimestamp, H.Activity FROM History H JOIN Car C ON H.CarID = C.CarID')
-    incidentData = execute_sql('Select I.IncidentID, C.LicensePlate, CUS.FirstName, CUS.LastName, I.Type, I.StartTimestamp, I.EndTimestamp, I.Status, U.FirstName, U.LastName, I.Description FROM Incident I JOIN Car C ON I.CarID = C.CarID JOIN Customer CUS ON C.CustomerID = CUS.CustomerID JOIN [User] U ON U.UserID = I.UserID')
-    carData = execute_sql('Select C.CarID, C.LicensePlate, CUS.FirstName, CUS.LastName, CUS.Phone, H.EnterTimestamp, I.Type FROM Car C JOIN Customer CUS ON C.CustomerID = CUS.CustomerID JOIN Incident I ON C.CarID = I.CarID JOIN History H ON C.CarID = H.CarID')
-    
+    historyData = cursor.execute('Select H.HistoryID, C.LicensePlate, H.EnterTimestamp, H.ExitTimestamp, H.Activity FROM History H JOIN Car C ON H.CarID = C.CarID')
+    historyData = cursor.fetchall()
+    incidentData = cursor.execute('Select I.IncidentID, C.LicensePlate, CUS.FirstName, CUS.LastName, I.Type, I.StartTimestamp, I.EndTimestamp, I.Status, U.FirstName, U.LastName, I.Description FROM Incident I JOIN Car C ON I.CarID = C.CarID JOIN Customer CUS ON C.CustomerID = CUS.CustomerID JOIN [User] U ON U.UserID = I.UserID')
+    incidentData = cursor.fetchall()
+    carData = cursor.execute('Select C.CarID, C.LicensePlate, CUS.FirstName, CUS.LastName, CUS.Phone, H.EnterTimestamp, I.Type FROM Car C JOIN Customer CUS ON C.CustomerID = CUS.CustomerID JOIN Incident I ON C.CarID = I.CarID JOIN History H ON C.CarID = H.CarID')
+    carData = cursor.fetchall()
     return render_template('home.html', historyData = historyData, incidentData = incidentData, carData = carData)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def logout():
-<<<<<<< HEAD
-=======
-    
->>>>>>> df66c71b6975167ffcaf630777e2799e9cd0a669
     return render_template('login.html')
