b6277e043f952188605a784df8d38242442c2da6
Nuntanutch T <opal454155@gmail.com>
2021-01-25 21:36:22 +0700

    loginAgain

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request, session
 import cv2
 import pyodbc
+from flask_login import LoginManager, login_required
 
 connection = pyodbc.connect('Driver={SQL Server};'
                             'Server=.;'
@@ -13,8 +14,13 @@ app = Flask(__name__, static_folder='static')
 app.secret_key = '123456789'
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET'])
 def login():
+    
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def do_login():
     error = ""
     username = request.form['username']
     password = request.form['password']
@@ -28,10 +34,10 @@ def login():
         session['lastname'] = account[4]
         session['type'] = account[5].lower()
         
-        return home()
+        # return render_template('home.html')
+        return redirect(url_for('home'))
     else:
         error = "Incorrect username or password!"
-    
     return render_template('login.html', error=error)
 
 @app.route('/blacklist')
@@ -39,30 +45,25 @@ def blacklist():
     return render_template('blacklist.html')
 
 
-@app.route('/user')
-def user():
-    return render_template('userHome.html')
-
-
 @app.route('/')
 def home():
     if not session.get('loggedin'):
         return render_template('login.html')
-    else:
-        if(session.get('type') != "admin"):
-            return render_template('userHome.html')
-        else:
 
-            historyData = cursor.execute('Select H.HistoryID, C.LicensePlate, H.EnterTimestamp, H.ExitTimestamp, H.Activity FROM History H JOIN Car C ON H.CarID = C.CarID')
-            historyData = cursor.fetchall()
-            incidentData = cursor.execute('Select I.IncidentID, C.LicensePlate, CUS.FirstName, CUS.LastName, I.Type, I.StartTimestamp, I.EndTimestamp, I.Status, U.FirstName, U.LastName, I.Description FROM Incident I JOIN Car C ON I.CarID = C.CarID JOIN Customer CUS ON C.CustomerID = CUS.CustomerID JOIN [User] U ON U.UserID = I.UserID')
-            incidentData = cursor.fetchall()
-            carData = cursor.execute('Select C.CarID, C.LicensePlate, CUS.FirstName, CUS.LastName, CUS.Phone, H.EnterTimestamp, I.Type FROM Car C JOIN Customer CUS ON C.CustomerID = CUS.CustomerID JOIN Incident I ON C.CarID = I.CarID JOIN History H ON C.CarID = H.CarID')
-            carData = cursor.fetchall()
-            return render_template('home.html', historyData=historyData, incidentData=incidentData, carData=carData)
+    historyData = cursor.execute('Select H.HistoryID, C.LicensePlate, H.EnterTimestamp, H.ExitTimestamp, H.Activity FROM History H JOIN Car C ON H.CarID = C.CarID')
+    historyData = cursor.fetchall()
+    incidentData = cursor.execute('Select I.IncidentID, C.LicensePlate, CUS.FirstName, CUS.LastName, I.Type, I.StartTimestamp, I.EndTimestamp, I.Status, U.FirstName, U.LastName, I.Description FROM Incident I JOIN Car C ON I.CarID = C.CarID JOIN Customer CUS ON C.CustomerID = CUS.CustomerID JOIN [User] U ON U.UserID = I.UserID')
+    incidentData = cursor.fetchall()
+    carData = cursor.execute('Select C.CarID, C.LicensePlate, CUS.FirstName, CUS.LastName, CUS.Phone, H.EnterTimestamp, I.Type FROM Car C JOIN Customer CUS ON C.CustomerID = CUS.CustomerID JOIN Incident I ON C.CarID = I.CarID JOIN History H ON C.CarID = H.CarID')
+    carData = cursor.fetchall()
+
+    if(session.get('type') != "admin"):
+        return render_template('userHome.html', historyData=historyData, incidentData=incidentData, carData=carData)
+    
+    return render_template('home.html', historyData=historyData, incidentData=incidentData, carData=carData)
 
 
-@app.route('/logout', methods=['GET'])
+@app.route('/logout', methods=['GET','POST'])
 def logout():
     session['loggedin'] = False
     return home()
